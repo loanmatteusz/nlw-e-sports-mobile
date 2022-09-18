565772ff3588c1e88cabe4888e07da2e41fe1eab
Loan Matteus <loan.matteusz@gmail.com>
2022-09-17 22:36:47 -0300

    [feat] data fetch games in api

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,11 +4,20 @@ import { Heading } from "../../components/Heading";
 import logoImg from '../../assets/logo-nlw-esports.png';
 
 import { styles } from './styles';
-import { GameCard } from "../../components/GameCard";
+import { GameCard, GameCardProps } from "../../components/GameCard";
 
 import { GAMES } from '../../utils/games';
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [games, setGames] = useState<GameCardProps[]>();
+
+  useEffect(() => {
+    fetch('http://127.0.1.1:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data));
+  }, []);
 
   return (
     <View>
@@ -22,7 +31,7 @@ export default function Home() {
       />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
           <GameCard
