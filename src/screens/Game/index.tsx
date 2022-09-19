618873e70b728b48f8a53e119f0e7a1f08817715
Loan Matteus <loan.matteusz@gmail.com>
2022-09-18 22:55:29 -0300

    [feat] add styles to emptyListText

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FlatList, Image, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { Entypo } from '@expo/vector-icons';
@@ -74,8 +74,13 @@ export function Game() {
           )}
           horizontal
           style={styles.containerList}
-          contentContainerStyle={styles.contentList}
+          contentContainerStyle={duos?.length > 0 ? styles.contentList : styles.emptyListContent}
           showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados ainda.
+            </Text>
+          )}
         />
       </SafeAreaView>
     </Background>
